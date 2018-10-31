81339197c33d569b6d8a984364dfb08b233517b6
Alejandro Quadri <alejandroquadri@gmail.com>
2018-10-31 16:48:12 -0300

    agregado transicion entre prerender y app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,6 @@ export class AppComponent {
     this.doc = document;
     this.win = window;
     this.getVH();
-    // this.analytics();
     this.analyticsS.trackRouter();
   }
 
--- src/app/shared/services/static.service.ts
+++ src/app/shared/services/static.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
+import { tap, startWith } from 'rxjs/operators';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+
 import { ApiService } from './api.service';
+
+// The state saved from the server render
+const DATA_KEY = makeStateKey<any>('quadri');
 
 @Injectable({
   providedIn: 'root'
@@ -7,22 +13,43 @@ import { ApiService } from './api.service';
 export class StaticService {
 
   data: any;
-  dataSubs: any;
+  data$: any;
 
   logo = './assets/icons/LogoQuadriFondoBlanco.png';
 
   constructor(
-    private api: ApiService
+    private api: ApiService,
+    private state: TransferState
   ) { }
 
+  // getStaticData() {
+  //   this.data$ = this.api.getObject('quadri');
+  //   return new Promise( (resolve, reject) => {
+  //     this.data$.subscribe( data => {
+  //       this.data = data;
+  //       console.log('static data available');
+  //       resolve(this.data);
+  //     });
+  //   });
+  // }
+
   getStaticData() {
-    this.dataSubs = this.api.getObject('quadri');
     return new Promise( (resolve, reject) => {
-      this.dataSubs.subscribe( data => {
+      const exists = this.state.get(DATA_KEY, {} as any);
+      this.data$ = this.api.getObject('quadri')
+      .pipe(
+        tap(obj => {
+          this.state.set(DATA_KEY, obj);
+        }),
+        startWith(exists)
+        )
+        .subscribe( data => {
         this.data = data;
-        console.log('static data available');
-        resolve(this.data);
+        if (!(Object.keys(this.data).length === 0 && this.data.constructor === Object)) {
+          resolve();
+        }
       });
     });
   }
+
 }
